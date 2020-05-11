68fe1d467fd91229d760d49fca76b3439f2ea614
Emma Leger <e.leger@codeur.online>
2020-05-11 15:14:43 +0200

    slider, navbar, smoothscroll

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include "header.php";
+ ?>
+
+<body class="background">
+
+<main class="background1">
+
+  <div id="carouselExampleControls" style="height: 80vh;" class="row align-items-center carousel slide" data-interval="false" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img.g/slider1.png" class="d-block w-100" alt="planche à découper + couteau">
+      </div>
+      <div class="carousel-item">
+        <img src="img.g/slider2.png" class="d-block w-100" alt="hamburger">
+      </div>
+      <div class="carousel-item">
+        <img src="img.g/slider3.png" class="d-block w-100" alt="cupcake">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="icon-bar">
+  <a href="#section1">1</a>
+  <a href="#section2">2</a>
+  <a href="#section3">3</a>
+</div>
+
+<div id="section1" class="vh-100">
+
+</div>
+<div id="section2" class="vh-100">
+
+</div>
+<div id="section3" class="vh-100">
+
+</div>
+
+</main>
+
+<?php
+include "footer.php";
